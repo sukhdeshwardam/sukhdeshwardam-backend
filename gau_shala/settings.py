e3e477e9581487bef871d63da130093fd66366b6
Prap1 <praphoolk32@gmail.com>
2026-03-29 20:28:34 +0530

    updated

--- gau_shala/settings.py
+++ gau_shala/settings.py
@@ -169,6 +169,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://sukhdeshwargaudham.vercel.app/",
 ]
 # If you need to allow all origins during development:
 # CORS_ALLOW_ALL_ORIGINS = True
