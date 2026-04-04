5ed6ed4d18a5beab11495240debdb0f88cda7cec
Prap1 <praphoolk32@gmail.com>
2026-04-04 13:02:10 +0530

    'updated'

--- gau_shala/urls.py
+++ gau_shala/urls.py
@@ -7,8 +7,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "status": "ok",
+        "message": "Gau Shala API is running"
+    })
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/auth/',    include('accounts.urls')),
     path('api/blog/',    include('blog.urls')),
